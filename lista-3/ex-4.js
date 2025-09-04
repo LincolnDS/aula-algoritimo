e1c16f885a2ee869bd31b7549055d935d43aeefa
LincolnDS <GabrielLincoln.23@outlook.com>
2025-09-04 00:56:14 +0000

    update

--- lista-3/ex-4.js
+++ lista-3/ex-4.js
@@ -7,8 +7,10 @@ let resp1 = num1 + 5
 let resp2 = num2 + 5 
 let resp3 = num3 + 5 
 
-if(resp1>10, resp2>10, resp3>10){
-    console.log("num1", resp1)
-    console.log("num2", resp2)
+if(resp1>10){ console.log("num1", resp1)}
+    if(resp2>10){ console.log("num2", resp2)}
+        if(resp3>10){
+   
+   
     console.log("num3", resp3)
 }
---
+++ lista-3/ex-9.js
@@ -0,0 +1,8 @@
+import input from 'readline-sync'
+let num1 = input.questionInt("digite um numero")
+num1 = num1 /2;
+if(num1>20){
+    console.log("o numero maior que 20 é",num1)
+
+
+}
