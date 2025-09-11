b95d1539269b9d45a5406a4dd1901df899c2f364
LincolnDS <GabrielLincoln.23@outlook.com>
2025-09-11 01:05:34 +0000

    Update

---
+++ lista-4/ex-3.js
@@ -0,0 +1,10 @@
+import input from 'readline-sync'
+let n1= input.questionInt("digite um numero")
+if(n1>10){
+    console.log("numero maior que 10:",n1)
+}
+else{
+    console.log ("numero menor ou igual a 10:", n1)
+}
+
+
