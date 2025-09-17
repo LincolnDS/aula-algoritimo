53aa69c5231ba4d7af400c5d477f3f3092d4b99a
LincolnDS <GabrielLincoln.23@outlook.com>
2025-09-17 03:43:29 +0000

    Update

--- lista-4/ex-8.js
+++ lista-4/ex-8.js
@@ -5,14 +5,14 @@ let num = input.questionInt("Digite um número: ")
 let resp = num + 5
 let resp1 = num + 20
 
-if(num<10 && resp1>25){
-        console.log("O resultado é: ", resp1)
+if(num<10-resp1>25){
+ console.log("O resultado é: ", resp1)
 }else{
-    if(num==10 && resp1>25){
-        console.log("O resultado é: ", resp1)
+if(num==10-resp1>25){
+ console.log("O resultado é: ", resp1)
     }
 else{
-    if(num>10 && resp>25)
-        console.log("O resultado é: ", resp)
+if(num>10-resp>25)
+console.log("O resultado é: ", resp)
 
 }}
--- lista-4/ex-9.js
+++ lista-4/ex-9.js
@@ -4,7 +4,6 @@ let num1 = input.questionInt("Digite o 1° Número: ");
 let num2 = input.questionInt("Digite o 2° Número: ");
 let num3 = input.questionInt("Digite o 3° Número: ");
 let num4 = input.questionInt("Digite o 4° Número: ");
-
 let resp1 = num1 + num2;
 let resp2 = num3 - num4;
 let resp3 = resp1 + resp2;
